9cd485c9d8a6e79c7f9a23e8a6518ed487608eb0
suskidee <109852676+suskidee@users.noreply.github.com>
2024-05-02 05:32:16 +0100

    fourth commit

--- TechWorld/settings.py
+++ TechWorld/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'TechWorld.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
     }
 }
 
